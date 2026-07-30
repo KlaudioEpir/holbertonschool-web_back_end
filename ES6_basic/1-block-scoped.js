bd35a5e22654e193206db587dda833c91205e101
KlaudioEpir <klajd.epir@gmail.com>
2026-07-30 17:49:28 +0200

    task-1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,9 +2,9 @@ export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
 
-  if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+if (trueOrFalse) {
+  let task = true;
+  let task2 = false;
   }
 
   return [task, task2];
