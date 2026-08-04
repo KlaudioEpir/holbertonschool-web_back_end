c63dd7d09eb0e3c12ea5f97521ce2b4fbe0371de
KlaudioEpir <klajd.epir@gmail.com>
2026-08-04 23:27:53 +0200

    task-9 fixed some correctons

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -7,3 +7,6 @@ export default function appendToEachArrayValue(array, appendString) {
 
   return array;
 }
+./U hoq var: var është i vjetëruar, ndaj u zëvendësua me const dhe let që janë më të sigurta../
+./U përdor for...of: Në vend të for...in (që iteronte mbi indeksat e vjetër), përdorëm for...of për të marrë direkt vlerat e listës./
+./U mbajt indeksi: Pasi for...of merret vetëm me vlerat, shtuam një variabël idx për të ruajtur pozicionin dhe për të modifikuar listën origjinale në vend (in-place)../
