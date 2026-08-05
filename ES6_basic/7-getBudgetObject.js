1e04b5fba828cfe7a3059c2f8a25a8230845904c
KlaudioEpir <klajd.epir@gmail.com>
2026-08-05 17:24:09 +0200

    changes

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -7,4 +7,4 @@ export default function getBudgetObject(income, gdp, capita) {
 
   return budget;
 }
-./Përdorimi i sintaksës shorthand është thjesht një mënyrë më e shkurtër për të krijuar objektin brenda funksionit. Mënyra se si funksioni eksportohet dhe importohet mbetet 100% e njëjtë./
+//Përdorimi i sintaksës shorthand është thjesht një mënyrë më e shkurtër për të krijuar objektin brenda funksionit. Mënyra se si funksioni eksportohet dhe importohet mbetet 100% e njëjtë./
