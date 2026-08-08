2457f5419e4187e7d925ee97af96cc90c579d89d
KlaudioEpir <klajd.epir@gmail.com>
2026-08-08 13:55:38 +0200

    task-8

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,6 +1,6 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
-
+0
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
